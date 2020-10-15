0944c30a2af47d328e21d92734e71b74b6b2de4b
dhmec <dharmoradiya@gmail.com>
2020-10-15 16:07:30 -0600

    add index.php and add table to index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My ToDo List</title>
+</head>
+<body>
+    <h1>My ToDo List</h1>
+
+
+    <h2>Thing to do<h2>
+    <table>
+        <tr>
+            <th>TaskCategory</th>    
+            <th>Task</th>
+            <th>DueDate</th>
+            <th>Complete</th>
+            <th>Delete</th>
+        </tr>
+        <!-- <?php echo $staff_members; ?> -->
+        
+    </table>
+    
+</body>
+</html>
